e30ee25dc306783a991adbf7a3f1120fdaf2f611
Adrian Sampson <adrian@radbox.org>
2014-05-19 21:33:56 -0700

    Check request type

--- hooknook.py
+++ hooknook.py
@@ -1,15 +1,14 @@
 import flask
 from flask import request, g
 import threading
-import enum
 import queue
 import click
+import os
 
 app = flask.Flask(__name__)
-
-
-class Command(enum.Enum):
-    echo = 1
+app.config.update(
+    DATA_DIR = os.path.expanduser(os.path.join('~', '.hooknook')),
+)
 
 
 class Worker(threading.Thread):
@@ -20,12 +19,11 @@ class Worker(threading.Thread):
 
     def run(self):
         while True:
-            args = self.queue.get()
-            self.handle(*args)
+            self.handle(self.queue.get())
 
-    def handle(command, *args):
-        if command == Command.echo:
-            print(args)
+    def handle(args):
+        print(args)
+        print(app.config['DATA_DIR'])
 
     def send(self, *args):
         self.queue.put(args)
@@ -39,16 +37,29 @@ def _setup():
 
 @app.route('/hook', methods=['POST'])
 def hook():
+    event_type = request.headers.get('X-GitHub-Event')
+    if not event_type:
+        log.info('Received a non-hook request')
+        return flask.jsonify(status='not a hook'), 403
+    elif event_type == 'ping':
+        return flask.jsonify(ping='pong')
+
+    # FIXME Validate GitHub request origin.
+
     payload = request.get_json()
-    g.worker.send(Command.echo, payload['repository']['name'])
+    app.logger.info(payload)
+    if 'repository' in payload:
+        g.worker.send(payload['repository']['owner'],
+                      payload['repository']['name'])
     return flask.jsonify(status='success')
 
 
 @click.command()
 @click.argument('host', default='0.0.0.0')
 @click.argument('port', default=5000)
-def run(host, port):
-    app.run(host=host, port=port)
+@click.option('--debug', '-d', is_flag=True)
+def run(host, port, debug):
+    app.run(host=host, port=port, debug=debug)
 
 
 if __name__ == '__main__':
